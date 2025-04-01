12b28de9440e7768c076b3372a1e43de942e5c8c
Umesh Kumar Verma <umeshkumarhzb145@gmail.com>
2025-04-01 09:01:04 +0530

    js conditional statment

---
+++ 04_Conditionals/index.js
@@ -0,0 +1,83 @@
+//***************Conditional Statement*************** */
+
+//if statement
+
+// let x = 10;
+
+// if(x >= 10){
+//     console.log('x is bigger ')
+// }else{
+//     console.log('x, is small')
+// }
+
+//using AND (&&) gate
+// let age = 20;
+// let hasID = true;
+
+// if (age >= 18 && hasID) {
+//     console.log("You are allowed to enter.");
+// } else {
+//     console.log("Access denied.");
+// }
+
+
+//Using OR (||) gate
+// let isWeekend = true;
+// let isHoliday = false;
+
+// if (isWeekend || isHoliday) {
+//     console.log("You can relax today.");
+// } else {
+//     console.log("It's a working day.");
+// }
+
+//Using Not (!) gate
+// let isRaining = false;
+
+// if (!isRaining) {
+//     console.log("You can go outside.");
+// } else {
+//     console.log("Take an umbrella.");
+// }
+
+
+//learn if else if statment
+// let age = 25;
+
+// if (age > 18 && age < 60) {
+//     console.log("You are an adult.");
+// } else if (age >= 60) {
+//     console.log("You are a senior citizen.");
+// } else {
+//     console.log("You are a minor.");
+// }
+
+
+//learn switch case  (learn more react,redux)
+// let day = 3;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
