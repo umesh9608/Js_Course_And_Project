a3c115a735f8bea5256b1213b9fb4af8c3354b72
Umesh Kumar Verma <umeshkumarhzb145@gmail.com>
2025-04-01 09:02:18 +0530

    js Loops

---
+++ 05_Loops/index.js
@@ -0,0 +1,23 @@
+//*****************loops******************* */
+
+// //for loop
+// let n = 2;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${n} X ${i} = ` ,n * i);
+// }
+
+
+// //while loop
+// let i = 1;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// //do....While loop
+// let j = 1;
+// do {
+//     console.log(i);
+//     j++;
+// } while (i <= 5);
