a72649264efb185f2e0c006ef23be22fa9f07989
Umesh Kumar Verma <umeshkumarhzb145@gmail.com>
2025-04-01 08:58:41 +0530

    js Variables & DataType

---
+++ 02_Variables_Data_types/index.js
@@ -0,0 +1,30 @@
+// console.log("Hello Data Types")
+
+//variables
+// varl
+// let
+// const
+
+//****************var******************* */
+// var x = 10;
+// var x = 20; //this is not error but have problems so now a day remove it in (2015)
+// console.log(x) //10
+// console.log(typeof(x)); //number
+
+
+// console.log(x)
+// var x = 20; //this is not error output show undefined
+
+
+// ********************let******************************
+// let x= 10;
+
+// // let x = 10;//this in error you can't d this
+// x = 20; //you can update this
+// console.log(x);
+
+
+//*********const**************************** */
+const cId = 2333; 
+// cId = 45333; //this is error (onece defined can't update)
+console.log(cId)
