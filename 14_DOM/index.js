317544c11c34bad1dfffc2c4e76b02b1ba751e53
Umesh Kumar Verma <umeshkumarhzb145@gmail.com>
2025-04-02 14:52:39 +0530

    Learn about DOM

---
+++ 14_DOM/index.js
@@ -0,0 +1,9 @@
+/*DOM introduction:- The Document object model is a proggramming iterfact for HTML and XML document. it represents the strcucture of a webpage as a tree of objects. allowing programming languages (like javaScript) to access, modify,and mainpulate the document's content,structure ad style */
+
+
+/* Accessing HTML elements
+. getElementById("myId")
+. getElementsByClassName("myClass")
+. getElementsByTagName("h1")
+. querySelector("div") //returns first element
+. querySelectorAll("div") //returns a NodeList*/
